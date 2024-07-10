b56259a0fe72880ff2d675b23f92fa84a8335add
October <letoctober@gmail.com>
2024-07-10 18:01:19 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,6 @@
+document.getElementById('containeronespanone').addEventListener('click', function() {
+    window.location.href = 'mailto:letoctober@gmail.com';
+});
+document.getElementById('containeronespantwo').addEventListener('click', function() {
+    window.location.href = 'https://github.com/letoctober';
+});
