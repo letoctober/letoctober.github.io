7f2560e9485c27f3571921c43c7d671a2854e41b
letoctober <letoctober@gmail.com>
2024-10-01 19:24:31 +0530

    updated website

--- logic.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('containeronespanone').addEventListener('click', function() {
-    window.location.href = 'mailto:letoctober@gmail.com';
-});
-document.getElementById('containeronespantwo').addEventListener('click', function() {
-    window.location.href = 'https://github.com/letoctober';
-});
